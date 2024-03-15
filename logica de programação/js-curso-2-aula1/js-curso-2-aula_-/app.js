6ea90f6a4f93d3c451cdaae2db4f975467dbf965
pierresouza <pierre.s3@hotmail.com>
2024-03-15 16:52:43 -0300

    :tada: feat: create repo and apply functions

---
+++ logica de programação/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,28 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou");
+    exibirTextoNaTela("p", "Você descobriu o número secreto");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "o número secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "o número secreto é maior");
+    }
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
